a30f2f9de4c36cc617722d5ed409b847b3a261e7
Anto <ant@onion.com>
2024-12-10 23:33:49 +0100

    un cambio de lógica en un punto muy critico para cumplir con la norma funcion ring_swap linea 23

--- libft/ring_array/ring_swap.c
+++ libft/ring_array/ring_swap.c
@@ -6,21 +6,21 @@
 /*   By: antofern <antofern@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 10:08:26 by antofern          #+#    #+#             */
-/*   Updated: 2024/12/10 17:38:34 by antofern         ###   ########.fr       */
+/*   Updated: 2024/12/10 23:26:50 by antofern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/ring.h"
 #include "../headers/libft.h"
 
-int ring_swap(t_ring *ring)// requiere % ring->slots en algún punto
+int	ring_swap(t_ring *ring)// requiere % ring->slots en algún punto
 {
 	int	tmp;
 
 	if (ring->fill < 2)
 		return (ERROR);
 	tmp = ring_get_head(ring);
-	ring->buff[ring->head] = ring->buff[ring_next_index(ring->head, ring->slots)];
+	ring->buff[ring->head] = ring_get_level(ring, 1);
 	ring->buff[ring_next_index(ring->head, ring->slots)] = tmp;
 	return (0);
 }
